df40a5617bf23ba2f44c56197968641b9e26cee6
fabiopisa <pisanellifabio@gmail.com>
2021-04-09 15:23:04 +0200

    commit resercizio finito in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ var numeri_stampati = []
 
 for(var i=0; i<101; i++){
   /* console.log(i) */
-  if(i % 3 == 0){
-    console.log("Fizz")
-    document.getElementById('stampa').innerHTML = "Fizz";
+  if(i % 3 == 0 && i % 5 ==0){
+    console.log(i + " FizzBuzz")
+    /* document.getElementById('stampa').innerHTML = "iFizz"; */
+  }else if(i % 3 ==0){
+    console.log(i + " Fizz")
+    /* document.getElementById('stampa').innerHTML += "Fizz"; */
   }else if(i % 5 ==0){
-    console.log("Buzz")
-    document.getElementById('stampa').innerHTML = "Buzz";
-  }else if(i % 3 == 0 && i % 5 == 0){
-    console.log("FizzBuzz");
-    document.getElementById('stampa').innerHTML = "FizzBuzz";
+    console.log(i + " Buzz");
+    /* document.getElementById('stampa').innerHTML += "Buzz"; */
+  }else{
+    console.log(i)
   }
 }
