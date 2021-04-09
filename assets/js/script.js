877a3235f58ebf1cbc1e4456dc8e98fa4d9649ab
fabiopisa <pisanellifabio@gmail.com>
2021-04-09 15:29:50 +0200

    commit esercizio finito con document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 var numeri_stampati = []
 
 for(var i=0; i<101; i++){
-  /* console.log(i) */
   if(i % 3 == 0 && i % 5 ==0){
-    console.log(i + " FizzBuzz")
-    /* document.getElementById('stampa').innerHTML = "iFizz"; */
+    console.log(i + " FizzBuzz");
+    document.getElementById('stampa').innerHTML += i + "," + " FizzBuzz; <br>";
   }else if(i % 3 ==0){
-    console.log(i + " Fizz")
-    /* document.getElementById('stampa').innerHTML += "Fizz"; */
+    console.log(i + " Fizz");
+    document.getElementById('stampa').innerHTML += i + "," + " Fizz; <br>";
   }else if(i % 5 ==0){
     console.log(i + " Buzz");
-    /* document.getElementById('stampa').innerHTML += "Buzz"; */
+    document.getElementById('stampa').innerHTML += i + "," + " Buzz; <br>";
   }else{
-    console.log(i)
+    console.log(i);
+    document.getElementById('stampa').innerHTML += i + ", <br>";
   }
 }
